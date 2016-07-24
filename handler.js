65a91235bbbd701e622bdecf49d5f068870c1b45
jnvm <j@nvm.io>
2016-07-24 20:02:25 +0000

    reworded summary & added more common callback names

--- handler.js
+++ handler.js
@@ -1,79 +1,79 @@
 module.exports=function init(opts){
-    var _=require("lodash")
-        ,introspect=require("introspect-fun")
+	var _=require("lodash")
+		,introspect=require("introspect-fun")
 
-    _.defaults(opts,{
-        beHelpful:false
-        ,methodMap:(key,value)=>_.castArray(
-            (key.match(/^(get|gather|find|fetch)/gi) || !_.isFunction(value) ? "GET"
-            : key.match(/POST|GET|PUT|PATCH|DELETE/i)
-            ) || "POST").first()
-        ,filter:(key,val)=>!(key[0]=='_' || val.priv || val.private)
-        ,source:{}
-        ,before:()=>{}
-        ,after:()=>{}
-        ,callbackNames:["done","next","then"]
-    })
+	_.defaults(opts,{
+		beHelpful:false
+		,methodMap:(key,value)=>_.castArray(
+			(key.match(/^(get|gather|find|fetch)/gi) || !_.isFunction(value) ? "GET"
+			: key.match(/POST|GET|PUT|PATCH|DELETE/i)
+			) || "POST").first()
+		,filter:(key,val)=>!(key[0]=='_' || val.priv || val.private)
+		,source:{}
+		,before:()=>{}
+		,after:()=>{}
+		,callbackNames:["done","next","then","cb","callback"]
+	})
 
-    //apply filter
-    var validVerb=_.keyBy(
-        _.flatten(
-            _.keys(opts.source)
-                .map(mdl=>_.keys(opts.source[mdl])
-                    .filter(k=>opts.filter(k,opts.source[mdl][k]))
-                )
-        )
-    )
+	//apply filter
+	var validVerb=_.keyBy(
+		_.flatten(
+			_.keys(opts.source)
+				.map(mdl=>_.keys(opts.source[mdl])
+					.filter(k=>opts.filter(k,opts.source[mdl][k]))
+				)
+		)
+	)
 
-    var Model=opts.source
-        ,beHelpful=opts.beHelpful
-        ,perform=function(req,res,target,done){
-            if(typeof target !=='function') done(target)
-            else{
-                //plug in inputs from req.query
-                //consider addressing non-last-input callback
-                var fxn=target
-                    ,args=introspect(fxn)
-                    ,callback=_.intersection(opts.callbackNames,args)[0]
-                    ,need=callback ? args.filter(n=>n!=callback) : args
-                    ,given=need.map(n=>req.query[n]).filter(v=>v!==undefined)
-                if(_.intersection(need,_.keys(req.query)).length!=need.length) res.status(400).json(beHelpful? "need: "+need : "arity mismatch")
-                try{
-                    if(callback) fxn(...given.concat( done ))
-                    else{
-                        var result=fxn(...given)
-                        if(result.constructor == (new Promise(()=>{})).constructor)
-                            result.then(resolved=>done(resolved))
-                                .catch(err=>{
-                                    res.status(500)
-                                    done({error})
-                                })
-                        else done(result)
-                    }
-                }
-                catch(error){res.status(500).json({error})}
-            }
-        }
+	var Model=opts.source
+		,beHelpful=opts.beHelpful
+		,perform=function(req,res,target,done){
+			if(typeof target !=='function') done(target)
+			else{
+				//plug in inputs from req.query
+				//consider addressing non-last-input callback
+				var fxn=target
+					,args=introspect(fxn)
+					,callback=_.intersection(opts.callbackNames,args)[0]
+					,need=callback ? args.filter(n=>n!=callback) : args
+					,given=need.map(n=>req.query[n]).filter(v=>v!==undefined)
+				if(_.intersection(need,_.keys(req.query)).length!=need.length) res.status(400).json(beHelpful? "need: "+need : "arity mismatch")
+				try{
+					if(callback) fxn(...given.concat( done ))
+					else{
+						var result=fxn(...given)
+						if(result.constructor == (new Promise(()=>{})).constructor)
+							result.then(resolved=>done(resolved))
+								.catch(err=>{
+									res.status(500)
+									done({error})
+								})
+						else done(result)
+					}
+				}
+				catch(error){res.status(500).json({error})}
+			}
+		}
 
-    return function(req,res,next){
-        var {model,verb}=req.params
-            ,error
-            ,method
-             if(beHelpful && !model) res.json(_.keys(_.pickBy(Model,_.isObject)))
-        else if(beHelpful && !verb) res.json(_.keys(Model[model]).filter(v=>validVerb[v]))
-        else{
-            //console.log({model,verb})
-            if(!model || !Model[model]) error=`${model} unavailable`
-            else if(verb && (!Model[model][verb] || !validVerb[verb])) error=`${model}.${verb} unavailable`
-            else if(verb && model && (method=opts.methodMap(verb,Model[model][verb])) !== req.method) error=`expected method ${method}`
-            if(error) res.status(400).json({error:beHelpful?error:""})
-            else{
-                opts.before(req,res)
-                perform(req,res,Model[model][verb],returned=>{
-                    opts.after(req,res,returned)
-                    res.json(returned)
-                })
-            }
-        }
-    }
+	return function(req,res,next){
+		var {model,verb}=req.params
+			,error
+			,method
+			 if(beHelpful && !model) res.json(_.keys(_.pickBy(Model,_.isObject)))
+		else if(beHelpful && !verb) res.json(_.keys(Model[model]).filter(v=>validVerb[v]))
+		else{
+			//console.log({model,verb})
+			if(!model || !Model[model]) error=`${model} unavailable`
+			else if(verb && (!Model[model][verb] || !validVerb[verb])) error=`${model}.${verb} unavailable`
+			else if(verb && model && (method=opts.methodMap(verb,Model[model][verb])) !== req.method) error=`expected method ${method}`
+			if(error) res.status(400).json({error:beHelpful?error:""})
+			else{
+				opts.before(req,res)
+				perform(req,res,Model[model][verb],returned=>{
+					opts.after(req,res,returned)
+					res.json(returned)
+				})
+			}
+		}
+	}
 }
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 # Auto API Middleware
 
-Do you want a subset of the methods on your backend classes/objects/models to be `fetch`-able or `$.ajax`-able on the front without having to write explicit express routes?
+Do you want all / a subset of the methods on your backend classes/objects/models/some `{}` to be `fetch`-able or `$.ajax`-able on the front without having to write explicit [express](https://github.com/expressjs/express) [routes](http://expressjs.com/en/guide/routing.html)?
 
 Then add them to your router like so:
 
@@ -8,25 +8,25 @@ Then add them to your router like so:
 .all("/json(/:model)?(/:verb)?", require("auto-api-middleware")({source:Model}) )
 ```
 
-##Example
-Say a Model was passed in that looked like this:
+## Example
+Say a `Model` was passed in that looked like this:
 
 ```javascript
 var Model={
-    book:{
-        getPage(num){}
-        ,getWordsByFrequency(isbn){}
-        ,fetchCover(isbn){}
-    }
-    ,scheduler:{
-        setAppointment(){}
-        ,getEventsInRange(start,end){}
-    }
-    ,user:{
-        get(key){}
-        ,update(key,changes){}
-        ,columns:{name:'',created:'',password:'',email:''}
-    }
+	book:{
+		getPage(num){}
+		,getWordsByFrequency(isbn){}
+		,fetchCover(isbn){}
+	}
+	,scheduler:{
+		setAppointment(){}
+		,getEventsInRange(start,end){}
+	}
+	,user:{
+		get(key){}
+		,update(key,changes){}
+		,columns:{name:'',created:'',password:'',email:''}
+	}
 }
 ```
 That would create these JSON routes:
@@ -40,7 +40,7 @@ That would create these JSON routes:
 /json/user/update
 /json/user/columns
 ```
-All routes for functions with named inputs would require them as query parameters like so:
+All routes for functions with named inputs would require them as URL query parameters like so:
 
 ```javascript
 $.ajax({url:"/json/scheduler/getEventsInRange",data:{start:"Tuesday",end:"Friday"}})
@@ -65,23 +65,23 @@ The options below allow that.
 //note the path is left to you to define, but :model and :verb are required somehow.
 //this path allows both to be optional, which makes it easier to beHelpful
 .all("/json(/:model)?(/:verb)?", require("auto-api-middleware")({
-    //opt:default value if you don't provide one
-    source:{} //should be shaped like {model1:{verbs},model2:{verbs},...}; compose as you'd like. Only take things at M.json.*?
-    ,beHelpful:false // suggest models, verbs, and inputs necessary for incomplete requests. Keep off in production.
-    ,filter(key,val){ //restrict which model properties to make available. Return truthy here to keep.
-        //key=verb, val=source[model][verb] in these cases
-        return !(key[0]=='_' || val.priv || val.private) //recall properties can be assigned to functions, if you want
-    }
-    ,methodMap(key,value){  // which HTTP method to use for which methods.
-        return _.castArray(
-            (key.match(/^(get|gather|find|fetch)/gi) || !_.isFunction(value) ? "GET"
-            : key.match(/POST|GET|PUT|PATCH|DELETE/i)
-            ) || "POST"
-        ).first()
-    }
-    ,before(req,res){} // sync run before verb. Say, force anything called user_id to be the session's user_id...
-    ,after(req,res,returned){} //sync run after verb to allow transforms of returned value before responding
-    ,callbackNames:["done","next","then"] //place the res.send callback in the first input named one of these
-                                          // (otherwise assume promise or sync result)
+	//opt:default value if you don't provide one
+	source:{} //should be shaped like {model1:{verbs},model2:{verbs},...}; compose as you'd like. Only take things at M.json.*?
+	,beHelpful:false // suggest models, verbs, and inputs necessary for incomplete requests. Keep off in production.
+	,filter(key,val){ //restrict which model properties to make available. Return truthy here to keep.
+		//key=verb, val=source[model][verb] in these cases
+		return !(key[0]=='_' || val.priv || val.private) //recall properties can be assigned to functions, if you want
+	}
+	,methodMap(key,value){  // which HTTP method to use for which methods.
+		return _.castArray(
+			(key.match(/^(get|gather|find|fetch)/gi) || !_.isFunction(value) ? "GET"
+			: key.match(/POST|GET|PUT|PATCH|DELETE/i)
+			) || "POST"
+		).first()
+	}
+	,before(req,res){} // sync run before verb. Say, force anything called user_id to be the session's user_id...
+	,after(req,res,returned){} //sync run after verb to allow transforms of returned value before responding
+	,callbackNames:["done","next","then","cb","callback"] //place the res.send callback in the first input named one of these
+										  // (otherwise assume promise or sync result)
 }) )
 ```
